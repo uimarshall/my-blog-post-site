0efc362fc57c50a7830c145885f960129809fdd9
uimarshall <uimarshall@gmail.com>
2023-06-21 23:33:19 +0100

    Add catchAsyncErrors middleware and errorHandler.

---
+++ backend/src/utils/errorHandler.ts
@@ -0,0 +1,16 @@
+/*
+ * message = error message
+ * statusCode = 404, etc.
+ * super = constructor method of the parent class(Error).
+ */
+
+// Error handler class
+class ErrorHandler extends Error {
+  constructor(public message: string, public statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = ErrorHandler;
