129f95dd66553650f71b263ad2d2232e8c9bfc0d
uimarshall <uimarshall@gmail.com>
2023-06-25 20:44:09 +0100

    Add admin privileges to user

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -31,4 +31,34 @@ const requireAuthentication = asyncHandler(async (req: Request, res: Response, n
   next();
 });
 
-export default requireAuthentication;
+// Handling user Roles - Admin
+
+const requireAdminRole = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  // A user will sign in first by running the requireAuthentication middleware and then we will check if the user is an admin or not.
+
+  const user = await User.findById(req.user?.id);
+  if (user?.isAdmin === true) {
+    next();
+  } else {
+    next(new ErrorHandler('You are not authorized to access this resource, Admin only', 403));
+  }
+});
+
+// Handling user Roles - Super Admin
+
+const requireAuthorizedRoles =
+  (...roles: string[]) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      next(
+        new ErrorHandler(
+          `Only admin can access this resource, your role: (${req.user.role}) is forbidden from accessing this resource!`,
+          403
+        )
+      );
+      return;
+    }
+    next();
+  };
+
+export { requireAuthentication, requireAuthorizedRoles, requireAdminRole };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { loginUser, logoutUser, protectedUser, registerUser } from '../controllers/userAuthController';
-import requireAuthentication from '../middlewares/authMiddleware';
+import { requireAdminRole, requireAuthentication } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-router.get('/auth', requireAuthentication, protectedUser);
+router.get('/auth', requireAuthentication, requireAdminRole, protectedUser);
 
 export default router;
